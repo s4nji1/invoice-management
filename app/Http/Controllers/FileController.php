afa73b28e6918242a0f391998b39b74584431302
s4nji1 <hamzaaitbouhou75@gmail.com>
2024-12-09 22:22:03 +0100

    third

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -9,12 +9,12 @@ class FileController extends Controller
 {
     public function show($filename){
 
-        $filePath = 'private/invoices/' . $filename;
+        $filePath = 'invoices/' . $filename;
 
         if (Storage::disk('local')->exists($filePath)) {
             return response()->file(storage_path('app/' . $filePath));
         }
-
+        
         return response()->json(['error' => 'File not found'], 404);
 
     }   
